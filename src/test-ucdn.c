981694ff125641ac17b2e916dcab7aac1ff69d35
Daniel Wolf <nephatrine@pos.nxp>
2016-09-24 17:37:38 -0400

    [ucdn] addded linebreak testcases

--- src/test-ucdn.c
+++ src/test-ucdn.c
@@ -2,75 +2,89 @@
 #include <stdlib.h>
 #include <ucdn.h>
 
+int report( uint32_t cp, uint32_t result, uint32_t expect )
+{
+	if( result != expect ) {
+		printf( "  - U+%04X\t= %d\t= EXPECTED %d\n", cp, result, expect );
+		return 1;
+	}
+
+	printf( "  - U+%04X\t= %d\t= SUCCESS\n", cp, result );
+	return 0;
+}
+
 int main( int argc, char *argv[] )
 {
+	int failures = 0;
+
 	printf( "unicode version: %s\n", ucdn_get_unicode_version() );
 
 	printf( "\ntesting combining classes\n" );
 
-	if( ucdn_get_combining_class( 0x035A ) != UCDN_COMBINING_BELOW ) {
-		printf( "  - 0x035A FAILED\n\n" );
-		return EXIT_FAILURE;
-	}
-	printf( "  - 0x035A = Below\n" );
+	uint32_t cc_expect[] = {0x035A, UCDN_COMBINING_BELOW,
+	                        0x035B, UCDN_COMBINING_ABOVE,
+	                        0x035C, UCDN_COMBINING_DOUBLE_BELOW,
+	                        0x035D, UCDN_COMBINING_DOUBLE_ABOVE};
 
-	if( ucdn_get_combining_class( 0x035B ) != UCDN_COMBINING_ABOVE ) {
-		printf( "  - 0x035B FAILED\n\n" );
-		return EXIT_FAILURE;
-	}
-	printf( "  - 0x035B = Above\n" );
-
-	if( ucdn_get_combining_class( 0x035C ) != UCDN_COMBINING_DOUBLE_BELOW ) {
-		printf( "  - 0x035C FAILED\n\n" );
-		return EXIT_FAILURE;
-	}
-	printf( "  - 0x035C = Double Below\n" );
-
-	if( ucdn_get_combining_class( 0x035D ) != UCDN_COMBINING_DOUBLE_ABOVE ) {
-		printf( "  - 0x035D FAILED\n\n" );
-		return EXIT_FAILURE;
-	}
-	printf( "  - 0x035D = Double Above\n" );
+	for( size_t i = 0; i < sizeof( cc_expect ) / sizeof( uint32_t ); i += 2 )
+		failures +=
+		        report( cc_expect[i], ucdn_get_combining_class( cc_expect[i] ),
+		                cc_expect[i + 1] );
 
 	printf( "\ntesting east asian width\n" );
 
-	if( ucdn_get_east_asian_width( 0x00AA ) != UCDN_EAST_ASIAN_AMBIGUOUS ) {
-		printf( "  - 0x00AA FAILED\n\n" );
-		return EXIT_FAILURE;
-	}
-	printf( "  - 0x00AA = Ambiguous\n" );
+	uint32_t ea_expect[] = {0x00AA, UCDN_EAST_ASIAN_AMBIGUOUS,
+	                        0x00AB, UCDN_EAST_ASIAN_NEUTRAL,
+	                        0x00AC, UCDN_EAST_ASIAN_NARROW};
 
-	if( ucdn_get_east_asian_width( 0x00AB ) != UCDN_EAST_ASIAN_NEUTRAL ) {
-		printf( "  - 0x00AB FAILED\n\n" );
-		return EXIT_FAILURE;
-	}
-	printf( "  - 0x00AB = Neutral\n" );
-
-	if( ucdn_get_east_asian_width( 0x00AC ) != UCDN_EAST_ASIAN_NARROW ) {
-		printf( "  - 0x00AC FAILED\n\n" );
-		return EXIT_FAILURE;
-	}
-	printf( "  - 0x00AC = Narrow\n" );
+	for( size_t i = 0; i < sizeof( ea_expect ) / sizeof( uint32_t ); i += 2 )
+		failures +=
+		        report( ea_expect[i], ucdn_get_east_asian_width( ea_expect[i] ),
+		                ea_expect[i + 1] );
 
 	printf( "\ntesting script\n" );
 
-	if( ucdn_get_script( 0x037E ) != UCDN_SCRIPT_COMMON ) {
-		printf( "  - 0x037E FAILED\n\n" );
-		return EXIT_FAILURE;
-	}
-	printf( "  - 0x037E = Common\n" );
+	uint32_t sc_expect[] = {0x037E, UCDN_SCRIPT_COMMON,
+	                        0x037F, UCDN_SCRIPT_GREEK,
+	                        0x0380, UCDN_SCRIPT_UNKNOWN};
 
-	if( ucdn_get_script( 0x037F ) != UCDN_SCRIPT_GREEK ) {
-		printf( "  - 0x037F FAILED\n\n" );
-		return EXIT_FAILURE;
-	}
-	printf( "  - 0x037F = Greek\n" );
+	for( size_t i = 0; i < sizeof( sc_expect ) / sizeof( uint32_t ); i += 2 )
+		failures += report( sc_expect[i], ucdn_get_script( sc_expect[i] ),
+		                    sc_expect[i + 1] );
 
-	if( ucdn_get_script( 0x0380 ) != UCDN_SCRIPT_UNKNOWN ) {
-		printf( "  - 0x037F FAILED\n\n" );
-		return EXIT_FAILURE;
-	}
-	printf( "  - 0x0380 = Unknown\n" );
+	printf( "\ntesting linebreak\n" );
 
+	uint32_t lb_expect[] = {
+	        0x000D, UCDN_LINEBREAK_CARRIAGE_RETURN,
+	        0x000A, UCDN_LINEBREAK_LINE_FEED,
+	        0x0085, UCDN_LINEBREAK_NEXT_LINE,
+	        0x00A7, UCDN_LINEBREAK_AMBIGUOUS,
+	        0x0E01, UCDN_LINEBREAK_COMPLEX_CONTEXT,
+	        0x0E31, UCDN_LINEBREAK_COMPLEX_CONTEXT,
+	        0x0E78, UCDN_LINEBREAK_UNKNOWN,
+	        0x3043, UCDN_LINEBREAK_CONDITIONAL_JAPANESE_STARTER};
+
+	for( size_t i = 0; i < sizeof( lb_expect ) / sizeof( uint32_t ); i += 2 )
+		failures +=
+		        report( lb_expect[i], ucdn_get_linebreak_class( lb_expect[i] ),
+		                lb_expect[i + 1] );
+
+	printf( "\ntesting simplified linebreak\n" );
+
+	uint32_t lbr_expect[] = {0x000D, UCDN_LINEBREAK_MANDATORY_BREAK,
+	                         0x000A, UCDN_LINEBREAK_MANDATORY_BREAK,
+	                         0x0085, UCDN_LINEBREAK_MANDATORY_BREAK,
+	                         0x00A7, UCDN_LINEBREAK_ALPHABETIC,
+	                         0x0E01, UCDN_LINEBREAK_ALPHABETIC,
+	                         0x0E31, UCDN_LINEBREAK_COMBINING_MARK,
+	                         0x0E78, UCDN_LINEBREAK_ALPHABETIC,
+	                         0x3043, UCDN_LINEBREAK_NONSTARTER};
+
+	for( size_t i = 0; i < sizeof( lbr_expect ) / sizeof( uint32_t ); i += 2 )
+		failures += report( lbr_expect[i],
+		                    ucdn_get_resolved_linebreak_class( lbr_expect[i] ),
+		                    lbr_expect[i + 1] );
+
+	if( failures > 0 ) return EXIT_FAILURE;
 	return EXIT_SUCCESS;
 }
